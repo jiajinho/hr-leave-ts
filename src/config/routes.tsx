37fefe978551ded4524a6ccd380ace1f70ab27b6
jiajinho <hojiajin7@gmail.com>
2022-04-27 17:55:44 +0800

    ActivityLog: Added skeleton page

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -3,12 +3,14 @@ import MyRequest from 'pages/MyRequest';
 import MyLeaveBal from 'pages/MyLeaveBal';
 import Settings from 'pages/Settings';
 import MyApprovals from 'pages/MyApprovals';
+import ActivityLog from 'pages/ActivityLog';
 
 import Home from 'components/svg/Home';
 import DocumentText from 'components/svg/DocumentText';
 import Gear from 'components/svg/Gear';
 import Database from 'components/svg/Database';
 import CheckCircle from 'components/svg/CheckCircle';
+import DocumentReport from 'components/svg/DocumentReport';
 
 export type Route = {
   display: {
@@ -23,80 +25,100 @@ export type Route = {
   routes?: { [k: string]: Route }
 }
 
-const routes: { [k: string]: Route } = {
-  dashboard: {
-    display: { sidebar: "Dashboard", header: "Dashboard" },
-    render: {
-      url: "/",
-      component: <Dashboard />
-    },
-    icon: Home
+const dashboard: Route = {
+  display: { sidebar: "Dashboard", header: "Dashboard" },
+  render: {
+    url: "/",
+    component: <Dashboard />
   },
-  myRequest: {
-    display: { sidebar: "My Request", header: "My Request" },
-    render: {
-      url: "/my-request",
-      component: <MyRequest.Main />
+  icon: Home
+}
+
+const myRequest: Route = {
+  display: { sidebar: "My Request", header: "My Request" },
+  render: {
+    url: "/my-request",
+    component: <MyRequest.Main />
+  },
+  icon: DocumentText,
+  routes: {
+    view: {
+      display: { header: "Leave Request Detail" },
+      render: { url: "/my-request/view/:id", component: <MyRequest.View /> }
     },
-    icon: DocumentText,
-    routes: {
-      view: {
-        display: { header: "Leave Request Detail" },
-        render: { url: "/my-request/view/:id", component: <MyRequest.View /> }
-      },
-      new: {
-        display: { header: "Leave Request Form" },
-        render: { url: "/my-request/new", component: <MyRequest.New /> }
-      }
+    new: {
+      display: { header: "Leave Request Form" },
+      render: { url: "/my-request/new", component: <MyRequest.New /> }
     }
+  }
+}
+
+const myApprovals: Route = {
+  display: { sidebar: "My Approvals", header: "My Approvals" },
+  render: {
+    url: "/my-approvals",
+    component: <MyApprovals.Main />
   },
-  myLeaveBal: {
-    display: { sidebar: "My Leave Balance", header: "My Leave Balance" },
-    render: {
-      url: "/my-leave-balance",
-      component: <MyLeaveBal />
+  icon: CheckCircle,
+  routes: {
+    pending: {
+      display: { header: "Pending Request Detail" },
+      render: { url: "/my-approvals/pending/:id", component: <MyApprovals.Pending /> }
     },
-    icon: Database
-  },
-  myApprovals: {
-    display: { sidebar: "My Approvals", header: "My Approvals" },
-    render: {
-      url: "/my-approvals",
-      component: <MyApprovals.Main />
-    },
-    icon: CheckCircle,
-    routes: {
-      pending: {
-        display: { header: "Pending Request Detail" },
-        render: { url: "/my-approvals/pending/:id", component: <MyApprovals.Pending /> }
-      },
-      approved: {
-        display: { header: "Approved Request Detail" },
-        render: { url: "/my-approvals/approved/:id", component: <MyApprovals.Approved /> }
-      }
+    approved: {
+      display: { header: "Approved Request Detail" },
+      render: { url: "/my-approvals/approved/:id", component: <MyApprovals.Approved /> }
     }
+  }
+}
+
+const activityLog: Route = {
+  display: { sidebar: "Activity Logs", header: "Activity Logs" },
+  render: {
+    url: "/activity-log",
+    component: <ActivityLog />
   },
-  settings: {
-    display: { sidebar: "Settings", header: "Settings" },
-    render: { url: "/settings" },
-    icon: Gear,
-    routes: {
-      general: {
-        display: { sidebar: "General", header: "General" },
-        render: {
-          url: "/settings/general",
-          component: <Settings.General />
-        }
-      },
-      working: {
-        display: { sidebar: "Working Day", header: "Working Day" },
-        render: {
-          url: "/settings/working",
-          component: <Settings.WorkingDay />
-        }
+  icon: DocumentReport
+}
+
+const myLeaveBal: Route = {
+  display: { sidebar: "My Leave Balance", header: "My Leave Balance" },
+  render: {
+    url: "/my-leave-balance",
+    component: <MyLeaveBal />
+  },
+  icon: Database
+}
+
+const settings: Route = {
+  display: { sidebar: "Settings", header: "Settings" },
+  render: { url: "/settings" },
+  icon: Gear,
+  routes: {
+    general: {
+      display: { sidebar: "General", header: "General" },
+      render: {
+        url: "/settings/general",
+        component: <Settings.General />
+      }
+    },
+    working: {
+      display: { sidebar: "Working Day", header: "Working Day" },
+      render: {
+        url: "/settings/working",
+        component: <Settings.WorkingDay />
       }
     }
   }
+}
+
+const routes: { [k: string]: Route } = {
+  dashboard,
+  myRequest,
+  myApprovals,
+  activityLog,
+  myLeaveBal,
+  settings
 }
 
 export default routes;
